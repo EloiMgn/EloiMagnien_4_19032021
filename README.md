c4f6af97641ccf7b4f726409b690b81a54177fbd
Eloi <eloi.magnien@gmail.com>
2021-03-23 18:22:41 +0100

    validations js prénom, nom, email, radios

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ Issues :
 
 2.  Implémenter entrées du formulaire :
     a. Lier les labels aux entrées dans le HTML en utilisant les attributs "for" et "id" dans le code existant. Corriger le code HTML quand nécessaire.
+    
     b. Utiliser du JavaScript pur (pas de jQuery) pour terminer le formulaire :
 
         - Le formulaire doit être valide quand l'utilisateur clique sur "Submit"
--- modal.js
+++ modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -16,10 +16,10 @@ const close = document.querySelector(".close");
 const first = document.getElementById("first");
 const last = document.getElementById("last");
 const email = document.getElementById("email");
-const date = document.getElementById("date");
+const date = document.getElementById("birthdate");
 const quantity = document.getElementById("quantity");
 const submitBtn = document.querySelector(".button");
-const textControl = document.querySelector(".text-control");
+const textControl = document.querySelectorAll(".text-control");
 
 // MODAL
 // launch modal event
@@ -40,35 +40,109 @@ function closeModal() {
 
 // DATA VALIDATIONS 
 
-/*  const firstLength = first.classList
- */ 
+let formValid = document.getElementById("submitBtn");
+let missFirst = document.getElementById("missFirst");
+let firstValid = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-"\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
 
-/* function firstLengthValidation (firstLength){
+let missLast = document.getElementById("missLast");
+let lastValid = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-"\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
 
-  const firstData = formData[0];
-  if (firstLength < 2){
-    formData.innertHTML = "<div>Prénom invalide, veuillez entrer un prénom de plus de 2 caractères</div>";
-    first.style.border = "solid, 3px, red";
-    return false 
-  } else{
-    return true
+let missEmail = document.getElementById("missEmail");
+let emailValid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+let missDate = document.getElementById("missDate");
+let dateValid = /^\d{2}-\d{2}-\d{4}$/;
+
+let missQuantity = document.getElementById("missQuantity");
+let quantityValid = /d/;
+
+let missLocation = document.getElementById("missLocation");
+let location1 = document.getElementById("location1");
+let location2 = document.getElementById("location2");
+let location3 = document.getElementById("location3");
+let location4 = document.getElementById("location4");
+let location5 = document.getElementById("location5");
+let location6 = document.getElementById("location6");
+            
+formValid.addEventListener("click", validation);
+
+function validation(event){
+  validationFirst(event);
+  validationLast(event);
+  validationEmail(event);
+  validationQuantity(event);
+  validationLocation(event);
+}
+//validation first 
+function validationFirst(event){
+    //Si le champ est vide ou incorrect
+    if (firstValid.test(first.value) == false){
+        event.preventDefault();
+        missFirst.textContent = "Le champ Prénom doit comporter un minimum de 2 caractères";
+        missFirst.style.color = "red";
+        missFirst.style.fontSize = "50%";
+        textControl[0].style.border = "solid red 3px";
+    }else{ 
+    }
+}
+// validation last
+function validationLast(event){
+  //Si le champ est vide ou incorrrect
+  if (lastValid.test(last.value) == false){
+      event.preventDefault();
+      missLast.textContent = "Le champ Nom doit comporter un minimum de 2 caractères";
+      missLast.style.color = "red";
+      missLast.style.fontSize = "50%";
+      textControl[1].style.border = "solid red 3px";
+  }else{ 
+  }
+}
+// validation email
+function validationEmail(event){
+  //Si le champ est vide ou incorrect
+  if (emailValid.test(email.value) == false){
+      event.preventDefault();
+      missEmail.textContent = "Veuillez entrer une adresse email valide";
+      missEmail.style.color = "red";
+      missEmail.style.fontSize = "50%";
+      textControl[2].style.border = "solid red 3px";
+  }else{ 
+  }
+}
+//validation date de naissance
+/* function validationDate(event){
+  //Si le champ est vide ou incorrect
+  if (dateValid.test(date.value) == false){
+      event.preventDefault();
+      missDate.textContent = "Veuillez entrer une date valide";
+      missDate.style.color = "red";
+      missDate.style.fontSize = "50%";
+      textControl[3].style.border = "solid red 3px";
+  }else{ 
   }
 } */
 
+// validation nombre de concours
 
-first.addEventListener('change', function(e) {
-  const firstData = formData[0];
-  var value = e.target.value;
-  if (value.length > 2 ) { 
-      isValid = true;
-      console.log(true)
-  } else {
-      firstData.innertHTML = "<div>Prénom invalide, veuillez entrer un prénom de plus de 2 caractères</div>";
-      firstData.style.border = "solid, 3px, red";
-      isValid = false;
-      console.log(false)
+ function validationQuantity(event){
+  //Si le champ est vide ou incorrrect
+  if (quantity.value =! 2){
+      event.preventDefault();
+      missQuantity.textContent = "Le champ  doit avoir un minimum de 2 caractères";
+      missQuantity.style.color = "red";
+      missQuantity.style.fontSize = "50%";
+      textControl[3].style.border = "solid red 3px";
+  }else{ 
   }
-});
+} 
 
-
-
+function validationLocation(event){
+  //Si le champ est vide ou incorrrect
+  if ((location1.checked == false)&&(location2.checked == false)&&(location3.checked == false)&&(location4.checked == false)&&(location5.checked == false)&&(location6.checked == false)){
+      event.preventDefault();
+      missLocation.textContent = "Veuillez sélectionner une option";
+      missLocation.style.color = "red";
+      missLocation.style.fontSize = "50%";
+  }else{ 
+  }
+}
